2186ef4dff9b09b2d8907394dc7f3b83931c7b64
Muhamad Abiyasa <abi@roots.co.id>
2021-04-13 14:45:51 +0700

    [abi] integrasi update profile

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const { verifyToken } = require('../middlewares')
 const router = express.Router()
 
 const userHandler = require('./handler/users')
 
 router.post('/register', userHandler.register)
 router.post('/login', userHandler.login)
+router.put('/', verifyToken, userHandler.update)
 
 module.exports = router
